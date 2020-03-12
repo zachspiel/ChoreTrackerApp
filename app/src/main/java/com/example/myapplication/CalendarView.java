20915143be7de6fde7cc3a2d890488d6bc3e2c8d
ZacharySpiel <zps9@nau.edu>
2020-03-12 15:18:33 -0700

    Added calendar view and simple navigation

---
+++ app/src/main/java/com/example/myapplication/CalendarView.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalendarView extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_monthly);
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button next = (Button) findViewById(R.id.button);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), CalendarView.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
     }
 }
