26fe083634c010fc612cf0666f39f46c29382424
Olivia Thoney <oliviapthoney@gmail.com>
2020-04-19 11:43:06 -0600

    Add child event classes

---
+++ ChoreTracker App/app/src/main/java/com/example/myapplication/repeatingEvent.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+public class repeatingEvent extends Event {
+    private repeatDates int[];
+
+    public repeatingEvent()
+    {
+
+    }
+
+    public reset()
+    {
+
+    }
+}
